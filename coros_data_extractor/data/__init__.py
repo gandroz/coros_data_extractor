cccc7afce6aae1a937ae6e3443ec691f9e9ac4e1
Enji Cooper <yaneurabeya@gmail.com>
2025-11-05 23:44:29 -0800

    Overhaul data export support added in 8e7add27401e6ff410baa91
    # Performance improvements
    
    The previous mechanism was extremely heavy handed in terms of how it
    exported data off the site: it would previously download the raw
    activity JSON just for the activity name and start date, then throw
    the rest of the activity data away. This wasted a lot of data (on the
    order of MB for larger activities) per export operation and added a lot
    more network overhead than was really needed.
    
    Take the data from the activity metadata, i.e., the `.get_activities()`
    derived values, and apply it to the exported activities instead.
    
    # Timeout modifications
    
    Adjust timeouts to better handle raw activity query API calls: now
    timeouts are split between simple API calls and the raw activity query
    API calls.
    
    # Limit exporting positional data to supporting activities
    
    Only select activities are available for export via GPX and KML file
    format as not all activity types, e.g., jumping rope, elliptical,
    gym cardio, etc, don't have positional data associated with them.
    There's no sense hitting the server if we know the activity type does
    not have positional data associated with it.
    
    # Expose query limits at the data extractor level
    
    Prior to this change all exports were stopped at the 200 entries mark.
    This was happening because the limit could not be adjusted from the
    export function as it consumed the data provided by the
    `.get_activities()` function. This makes it easier to tune the
    behavior without having to resort to specifying the limit in each
    high-level API call provided by the data module.
    
    Signed-off-by: Enji Cooper <yaneurabeya@gmail.com>

--- coros_data_extractor/data/__init__.py
+++ coros_data_extractor/data/__init__.py
@@ -10,20 +10,23 @@ import hashlib
 import json
 import logging
 import math
+import pathlib
 import time
-from pathlib import Path
+import typing
 
 import requests
 
-from ..model import (
+from coros_data_extractor.model import (
     Frequencies,
     Lap,
     Summary,
     TrainActivities,
     TrainActivity,
 )
+
 from .api_model import (
     ActivityFileType,
+    ActivityType,
     LapType,
 )
 from .constants import (
@@ -31,9 +34,11 @@ from .constants import (
     ACTIVITY_DETAILS_URL,
     ACTIVITY_DOWNLOAD_URL,
     ACTIVITY_PAGINATION_LIMIT,
-    API_TIMEOUT,
-    DEFAULT_ACTIVITY_LIMIT,
+    DEFAULT_ACTIVITY_QUERY_LIMIT,
+    GET_ACTIVITY_QUERY_TIMEOUT,
     LOGIN_URL,
+    RAW_ACTIVITY_QUERY_API_TIMEOUT,
+    SIMPLE_QUERY_API_TIMEOUT,
 )
 
 # ruff: noqa: S324
@@ -51,11 +56,15 @@ WAIT_BETWEEN_RETRIES = 0.5
 class CorosDataExtractor:
     """Coros data extractor from Training Hub."""
 
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        default_activity_query_limit: int | None = DEFAULT_ACTIVITY_QUERY_LIMIT,
+    ) -> None:
         """Initialize extractor."""
         self.access_token = None
         self.activities = None
         self.user_id = None
+        self.default_activity_query_limit = default_activity_query_limit
 
     def login(self, account: str, password: str) -> None:
         """Login to Coros API.
@@ -71,7 +80,11 @@ class CorosDataExtractor:
             "accountType": 2,
             "pwd": hashlib.md5(password.encode()).hexdigest(),
         }
-        resp = requests.post(LOGIN_URL, json=request_data, timeout=API_TIMEOUT)
+        resp = requests.post(
+            LOGIN_URL,
+            json=request_data,
+            timeout=SIMPLE_QUERY_API_TIMEOUT,
+        )
         resp.raise_for_status()
         data_wrapped = resp.json()["data"]
         self.access_token = data_wrapped["accessToken"]
@@ -80,30 +93,51 @@ class CorosDataExtractor:
     def export_activities(
         self,
         file_type: ActivityFileType,
+        output_directory: pathlib.Path,
+        activity_types: ... = None,
+        limit: int | None = None,
     ) -> None:
         """Export activities of a specific file type from the Coros API.
 
         Args:
-            file_type: a file type to export data for, e.g., ActivityFileType.GPX.
+            file_type:          a file type to export data for, e.g.,
+                                `ActivityFileType.GPX`.
+            output_directory:   the directory to export the data to. This path must
+                                already exist.
+            activity_types:     Activity types to export data for.
+            limit:              The maximum number of activities to export.
 
         """
         with requests.Session() as dl_session, requests.Session() as query_session:
-            self._export_activities_inner(dl_session, query_session, file_type)
+            self._export_activities_inner(
+                dl_session,
+                query_session,
+                file_type,
+                output_directory,
+                activity_types=activity_types,
+                limit=limit,
+            )
 
     def _export_activities_inner(
         self,
         download_session: requests.Session,
         query_session: requests.Session,
         file_type: ...,
+        output_directory: ...,
+        activity_types: ... = None,
+        limit: ... = None,
     ) -> None:
         """Export activities of a specific file type from the Coros API (inner).
 
         Args:
             download_session: the HTTP session which will be used to download the
                               exported activities.
-            query_session: the HTTP session which will be used to query for
-                           exported activities.
-            file_type: a file type to export data for, e.g., ActivityFileType.GPX.
+            query_session:    the HTTP session which will be used to query for
+                              exported activities.
+            file_type:        see description under `.export_activities(..)`.
+            output_directory: see description under `.export_activities(..)`.
+            activity_types:   see description under `.export_activities(..)`.
+            limit:            see description under `.export_activities(..)`.
 
         """
         match file_type:
@@ -118,36 +152,50 @@ class CorosDataExtractor:
             case ActivityFileType.TCX:
                 extension = "tcx"
 
-        activities = self.get_activities()
+        activities = self.get_activities(
+            activity_types=activity_types,
+            limit=limit,
+        )
         headers = {"Accesstoken": self.access_token}
 
         for activity in activities:
-            # extract raw data of an activity
+            activity_name = activity["name"]
+            activity_start_time = activity["startTime"]
             label_id = activity["labelId"]
+            sport_type_raw = activity["sportType"]
             try:
-                activity_data = self.get_raw_activity_data(
-                    session=query_session, activity=activity,
+                sport_type = ActivityType(sport_type_raw)
+            except Exception:
+                LOGGER.debug(
+                    "Sport type=%d not supported by ActivityType. Metadata: "
+                    "startTimestamp=%r, name=%r, label_id=%r",
+                    sport_type_raw,
+                    activity_start_time,
+                    activity_name,
+                    label_id,
+                    exc_info=True,
                 )
-            except (requests.RequestException, RuntimeError):
-                LOGGER.exception(
-                    "Encountered error when processing activity, %r; continuing...",
-                    activity,
-                )
-                continue
             else:
-                activity_summary = self.get_summary_data(
-                    activity_data["data"]["summary"],
-                )
+                if not sport_type.supports_export(file_type):
+                    LOGGER.debug(
+                        "Skipping file export for ActivityType with label_id=%r; "
+                        "does not support file type=%s",
+                        label_id,
+                        file_type.name,
+                    )
+                    continue
 
-            sport_type = activity["sportType"]
+            # https://teamapi.coros.com/activity/query?size=10&pageNumber=1&modeList=
             payload = {
                 "labelId": label_id,
                 "fileType": file_type.value,
-                "sportType": sport_type,
+                "sportType": sport_type_raw,
             }
 
             resp = query_session.post(
-                ACTIVITY_DOWNLOAD_URL, headers=headers, data=payload,
+                ACTIVITY_DOWNLOAD_URL,
+                headers=headers,
+                data=payload,
             )
             resp.raise_for_status()
             resp_json = resp.json()
@@ -163,29 +211,29 @@ class CorosDataExtractor:
                 LOGGER.info(
                     "Could not download %s file type; is it supported with sport "
                     "type=%s? Response from server: %s",
-                    file_type.name, sport_type, resp_json,
+                    file_type.name,
+                    sport_type,
+                    resp_json,
                 )
                 continue
 
             download_url = resp_json["data"]["fileUrl"]
             resp = download_session.get(download_url, stream=True)
-            filename = (
-                f"{activity_summary.startTimestamp.isoformat()}_{activity_summary.name}_{label_id}.{extension}"
-            )
+            filename = f"{activity_start_time}_{activity_name}_{label_id}.{extension}"
 
             LOGGER.debug(
-                "Downloading file with %d from %s to %s",
-                label_id, download_url, filename,
+                "Downloading file with %s from %s to %s",
+                label_id,
+                download_url,
+                filename,
             )
-
-            with (Path("exports") / filename).open("wb") as fp:
-                fp.write(resp.raw.read())
+            (output_directory / filename).write_bytes(resp.raw.read())
 
     def get_activities(
         self,
-        limit: int | None = DEFAULT_ACTIVITY_LIMIT,
-        activity_types: list[int] | None = None,
-    ) -> dict:
+        activity_types: list[ActivityType] | None = None,
+        limit: int | None = None,
+    ) -> list[dict]:
         """Extract list of activities from API.
 
         Args:
@@ -196,28 +244,37 @@ class CorosDataExtractor:
         """
         with requests.Session() as session:
             return self._get_activities_inner(
-                session, limit=limit, activity_types=activity_types,
+                session,
+                activity_types=activity_types,
+                limit=limit,
             )
 
     def _get_activities_inner(
         self,
         session: requests.Session,
-        limit: ...,
         activity_types: ...,
+        limit: ...,
     ) -> dict:
         """Extract list of activities from API (inner).
 
         Args:
-            session: HTTP session used to query for activities.
-            limit: a maximum number of activities to get in a single query.
+            session:        HTTP session used to query for activities.
             activity_types: a list of types to get activities for, or `None` to
                             fetch all activities.
+            limit:          a maximum number of activities to get in a single query.
+
+        Returns:
+            Activity data as presented by Coros, i.e., the "data" -> "dataList" key in
+            the raw JSON.
 
         """
         if activity_types is None:
             mode_list = ""
         else:
             mode_list = ",".join(str(activity_type) for activity_type in activity_types)
+
+        if limit is None:
+            limit = self.default_activity_query_limit
 
         payload = {
             "modeList": mode_list,
@@ -232,7 +289,10 @@ class CorosDataExtractor:
             # given activity type. This allows you to pull the data in chunks.
             payload["size"] = 1
             resp = session.get(
-                ACTIVITIES_URL, headers=headers, params=payload, timeout=API_TIMEOUT,
+                ACTIVITIES_URL,
+                headers=headers,
+                params=payload,
+                timeout=GET_ACTIVITY_QUERY_TIMEOUT,
             )
             resp.raise_for_status()
             res = resp.json()
@@ -255,7 +315,7 @@ class CorosDataExtractor:
                 ACTIVITIES_URL,
                 headers=headers,
                 params=payload,
-                timeout=API_TIMEOUT,
+                timeout=SIMPLE_QUERY_API_TIMEOUT,
             )
             resp.raise_for_status()
             res = resp.json()
@@ -303,7 +363,8 @@ class CorosDataExtractor:
         for retries_left in range(max_tries - 1, -1, -1):
             try:
                 resp_json = self._get_raw_activity_data_inner(
-                    session, activity,
+                    session,
+                    activity,
                 )
             except Exception:
                 LOGGER.exception("An exception occurred when downloading the raw JSON")
@@ -355,7 +416,10 @@ class CorosDataExtractor:
         }
         headers = {"Accesstoken": self.access_token}
         resp = session.post(
-            ACTIVITY_DETAILS_URL, headers=headers, params=payload, timeout=API_TIMEOUT,
+            ACTIVITY_DETAILS_URL,
+            headers=headers,
+            params=payload,
+            timeout=RAW_ACTIVITY_QUERY_API_TIMEOUT,
         )
         resp.raise_for_status()
         return resp.json()
@@ -386,30 +450,64 @@ class CorosDataExtractor:
         return Summary(**data)
 
     @staticmethod
-    def get_laps_data(data: dict) -> list[Lap]:
-        """Convert raw activity to laps data."""
+    def get_laps_data(
+        sport_type: ActivityType,
+        raw_lap_data: dict[str, typing.Any],
+    ) -> list[Lap]:
+        """Convert raw activity to laps data.
+
+        Args:
+            sport_type:     The sport type for the activity data.
+            raw_lap_data:   The raw lap activity data.
+
+        Returns:
+            A list of Lap objects for the provided activity.
+
+        """
+        if sport_type not in ActivityType.RUN_ACTIVITY_TYPES:
+            return []
+
         laps = []
-        for item in data:
+        for item in raw_lap_data:
             if item["type"] == LapType.RUNNING:
                 laps.extend(Lap(**lap) for lap in item["lapItemList"])
             # XXX: item["type"] == LapType.BIKING:
         return laps
 
-    def extract_data(self) -> None:
+    def extract_data(
+        self,
+        activity_types: ... = None,
+        limit: ... = None,
+    ) -> None:
         """Extract data from Coros API & build data models accordingly."""
         with requests.Session() as session:
-            self._extract_data_inner(session)
+            self._extract_data_inner(
+                session,
+                activity_types=activity_types,
+                limit=limit,
+            )
 
-    def _extract_data_inner(self, session: requests.Session) -> None:
+    def _extract_data_inner(
+        self,
+        session: requests.Session,
+        activity_types: ... = None,
+        limit: ... = None,
+    ) -> None:
         """Extract data from Coros API & build data models accordingly (inner)."""
-        # get all activites
-        activities = self.get_activities()
+        # Get activites
+        activities = self.get_activities(
+            activity_types=activity_types,
+            limit=limit,
+        )
         self.activities = TrainActivities()
         for activity in activities:
-            # extract raw data of an activity
+            sport_type = activity["sportType"]
+
+            # Extract raw data of an activity.
             try:
                 activity_data = self.get_raw_activity_data(
-                    session=session, activity=activity,
+                    session=session,
+                    activity=activity,
                 )
             except (requests.RequestException, RuntimeError):
                 LOGGER.exception(
@@ -418,27 +516,38 @@ class CorosDataExtractor:
                 )
                 continue
 
-            # build pydantic models
+            # Build pydantic models for the activity.
             try:
                 data_wrapped = activity_data["data"]
                 training_activity = TrainActivity(
-                    summary=CorosDataExtractor.get_summary_data(data_wrapped["summary"]),
-                    data=CorosDataExtractor.get_activity_data(data_wrapped["frequencyList"]),
-                    laps=CorosDataExtractor.get_laps_data(data_wrapped["lapList"]),
+                    summary=self.get_summary_data(data_wrapped["summary"]),
+                    data=self.get_activity_data(data_wrapped["frequencyList"]),
+                    laps=self.get_laps_data(sport_type, data_wrapped["lapList"]),
                 )
             except KeyError:
                 LOGGER.exception(
                     "Encountered error when processing activity, %r; continuing...",
-                    data_wrapped,
+                    activity,
                 )
             else:
                 self.activities.add_activity(training_activity)
 
-    def to_json(self, filename: str = "activities.json") -> None:
-        """Export data to a JSON file."""
+    def to_json(
+        self,
+        filename: str,
+    ) -> None:
+        """Export data to a JSON file.
+
+        Args:
+            filename:           the export filename (basename only).
+
+        """
         if self.activities is None:
-            LOGGER.debug("No activities have been imported [yet].")
+            LOGGER.debug(
+                "No activities have been imported [yet]. "
+                "Hint: call `.extract_data(..)` first.",
+            )
             return
 
-        with Path(filename).open("w") as f:
-            json.dump(self.activities.model_dump(), f, indent=2)
+        with pathlib.Path(filename).open("w") as file_obj:
+            json.dump(self.activities.model_dump(), file_obj, indent=2)
